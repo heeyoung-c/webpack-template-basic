a72108c17e946294aeebf398a4e9e2c19469dfe6
heeyoung-c <heeyoungchun.dev@gmail.com>
2022-04-22 15:12:33 +0900

    feat: Create Webpack-Template-Basic

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+// import
+const path = require('path')
+const HtmlPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
+// export
+module.exports = {
+  // 파일을 읽어들이기 시작하는 진입점 설정
+  entry: './js/main.js',
+
+  // 결과물(번들)을 반환하는 설정
+  output: {
+    // path: path.resolve(__dirname, 'dist'),
+    // filename: 'main.js',
+    // 위의 값은 기본값이다
+    clean: true
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        use: [
+          'babel-loader'
+        ]
+      }
+    ]
+  },
+
+  // 번들링 후 결과물의 처리 방식 등 다양한 플러그인들을 설정
+  plugins: [
+    new HtmlPlugin({
+      template: './index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'static' }
+      ]
+    })
+  ]
+
+  // 개발 웹주소가 제대로 명시되지 않을 때 활성화
+  // devServer: {
+  //   host: 'localhost'
+  // }
+}
